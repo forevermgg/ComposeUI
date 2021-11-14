c4b1cb1aea36407354a46222e3962c90073c9c26
forevermeng <forevermeng@tencent.com>
2021-11-14 11:24:19 +0800

    优化

--- buildSrc/src/main/kotlin/AndroidLibraryPlugin.kt
+++ buildSrc/src/main/kotlin/AndroidLibraryPlugin.kt
@@ -1,5 +1,7 @@
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
 
 class AndroidLibraryPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -8,5 +10,16 @@ class AndroidLibraryPlugin : Plugin<Project> {
         project.addPlugin(AndroidConfig.Plugin.kapt)
         project.androidConfiguration { defaultConfig() }
         project.kotlinCompileOptions()
+    }
+
+    /**
+     * Configures the [Java][org.gradle.api.plugins.JavaPluginExtension] extension.
+     */
+    private fun applyJavaExtension(project: Project) {
+        val extension = project.extensions.findByType(JavaPluginExtension::class.java) ?: return
+        extension.apply {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
     }
 }
